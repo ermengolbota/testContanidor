bcc6251fda032dbdb53df4d10b384a6d3996dff7
Ermengol <ebota@inspedralbes.cat>
2022-03-09 11:11:16 +0100

    bàsic

---
+++ src/test.php
@@ -0,0 +1,8 @@
+<h1>Prova dins de SRC</h1>
+<?php
+echo "hola, soc una prova";>
+?>
+<?php
+phpinfo();
+?>
+<h1>FI</h1>
---
+++ test.php
@@ -0,0 +1,8 @@
+<h1>Prova 1</h1>
+<?php
+echo "hola, soc una prova";>
+?>
+<?php
+phpinfo();
+?>
+<h1>FI</h1>
